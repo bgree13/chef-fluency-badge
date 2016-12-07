ed4ae139d72d17eae9ccb992f3968b1255452d67
root <root@bgreen1.mylabserver.com>
2016-12-07 21:12:52 +0000

    Added motd and changed apache efault for node attributes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,6 +5,12 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 
+if node['platform_family'] == "rhel"
+	package ="httpd"
+elsif node['platform_family'] == "debian"
+	package ="apache2"
+end
+
 package 'apache2' do
 	package_name 'httpd'
 	action :install
@@ -14,3 +20,4 @@ service 'apache2' do
 	service_name 'httpd'
 	action [:start, :enable]
 end
+
